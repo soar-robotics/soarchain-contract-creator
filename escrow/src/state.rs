e22a68939d30bebb13eadb6c3be4087e643eccab
golnar-m <golnar@soarrobotics.com>
2024-01-19 11:41:51 +0300

    refactor(escrow): enhance state management in state.rs

--- escrow/src/state.rs
+++ escrow/src/state.rs
@@ -1,11 +1,18 @@
 use cosmwasm_std::{ Order, StdResult, Storage};
-use cw_storage_plus::Map;
+use cw_storage_plus::{Map, Item};
 use cosmwasm_std::{Addr, Uint128};
+use serde::{Serialize, Deserialize};
 
 use crate::escrow::Escrow;
 
 pub const ESCROWS: Map<&str, Escrow> = Map::new("escrow");
 pub const BALANCES: Map<&Addr, Uint128> = Map::new("balance");
+pub const STATE: Item<State> = Item::new("state");
+
+#[derive(Serialize, Deserialize)]
+pub struct State {
+    pub owner: String,
+}
 
 /// This returns the list of ids for all registered escrows
 pub fn all_escrow_ids(storage: &dyn Storage) -> StdResult<Vec<String>> {
