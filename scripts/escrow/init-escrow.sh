551c23ec4504666a9df05daf957b9a5f6e0c29be
golnar-m <golnar@soarrobotics.com>
2024-01-19 14:22:06 +0300

    feat(scripts): enhance init-escrow.sh to support token denomination.In the init-escrow.sh script under the scripts/escrow directory, enhance the script to accept a token denomination as a parameter during contract initialization. Changes include: - Add a DENOM parameter to the script.Modify the INIT JSON structure to include the denom field with the provided token denomination. This modification enables users to specify the token denomination during the initiation of the escrow contract, offering greater flexibility.

--- scripts/escrow/init-escrow.sh
+++ scripts/escrow/init-escrow.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 CODE_ID=$1
-INIT={}
+DENOM=$2
+
+INIT='{'\
+'"denom": "'"$DENOM"'"'\
+'}';
 
 $CHAIN tx wasm instantiate $CODE_ID "$INIT" --label "SOARCHAIN ESCROW INIT" --no-admin --from $ACCOUNT --node $NODE --chain-id $CHAINID --gas-prices 0.1$DENOM --gas auto --gas-adjustment 1.3 -b block -y
 
