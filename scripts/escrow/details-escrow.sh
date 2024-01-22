8ac7a70a3140e888ca2f1138df769ef21ff2aea5
golnar-m <golnar@soarrobotics.com>
2024-01-23 01:43:28 +0300

    chore: Update withdrawal script with node and chain information

--- scripts/escrow/details-escrow.sh
+++ scripts/escrow/details-escrow.sh
@@ -2,4 +2,4 @@
 
 ID=$1
 
-$CHAIN query wasm contract-state smart $ESCROW_CONTRACT_ADDRESS '{"details":{"id":"'"$ID"'"}}' --node $NODE
+$CHAIN query wasm contract-state smart $ESCROW_CONTRACT_ADDRESS '{"details":{"id":"'"$ID"'"}}' --node $NODE --chain-id $CHAINID 
