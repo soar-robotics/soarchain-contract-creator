e1c08f103d79a6383d3b5794b715444a25198313
golnar-m <golnar@soarrobotics.com>
2024-01-23 01:44:02 +0300

    chore: Update deploy  script with node and chain information

--- scripts/escrow/deploy-escrow.sh
+++ scripts/escrow/deploy-escrow.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-$CHAIN tx wasm store ./escrow/artifacts/escrow.wasm --from $ACCOUNT --node $NODE --gas-prices 0.1$DENOM --gas auto --gas-adjustment 1.3 -b block -y
+$CHAIN tx wasm store ./escrow/artifacts/escrow.wasm --from $ACCOUNT --node $NODE --chain-id $CHAINID --gas-prices 0.1$DENOM --gas auto --gas-adjustment 1.3 -b block -y
