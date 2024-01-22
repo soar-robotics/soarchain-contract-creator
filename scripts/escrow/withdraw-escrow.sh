a26a9b83dff863262d719a4be5038c5e43239c2e
golnar-m <golnar@soarrobotics.com>
2024-01-23 01:38:42 +0300

    chore: Update withdrawal script with node and chain information

--- scripts/escrow/withdraw-escrow.sh
+++ scripts/escrow/withdraw-escrow.sh
@@ -6,4 +6,4 @@ SECRET=$3
 
 WITHDRAW_DATA='{"withdraw":{"id": "'"$ID"'", "secret":"'"$SECRET"'"}}'
 
-$CHAIN tx wasm execute $ESCROW_CONTRACT_ADDRESS "$WITHDRAW_DATA" --gas-prices 0.025udmotus --from $RIDER --gas auto --gas-adjustment 1.5 -b block -y
+$CHAIN tx wasm execute $ESCROW_CONTRACT_ADDRESS "$WITHDRAW_DATA" --gas-prices 0.025udmotus --from $RIDER --node $NODE --chain-id $CHAINID --gas auto --gas-adjustment 1.5 -b block -y
